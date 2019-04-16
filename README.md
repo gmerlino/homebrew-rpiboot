e55cbc4c705236c76e45fd73a7350662f273eb42
Giovanni Merlino <gmerlino@unime.it>
2019-04-16 21:00:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew formula for rpiboot (Raspberry Pi USB booting code)
+## How do I install this formula?
+`brew tap gmerlino/rpiboot` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/gmerlino/homebrew-rpiboot/master/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://github.com/Homebrew/brew/tree/master/share/doc/homebrew#readme).
---
+++ rpiboot.rb
@@ -0,0 +1,38 @@
+class Rpiboot < Formula
+  desc "Raspberry Pi USB booting code"
+  homepage "https://github.com/raspberrypi/usbboot"
+
+  head do
+    url "https://github.com/raspberrypi/usbboot.git"
+
+    depends_on "libusb" => :build
+    depends_on "pkg-config" => :build
+  end
+
+  patch :p0, :DATA
+
+  def install
+    ENV.append_path "PKG_CONFIG_PATH", Formula["libusb"].opt_lib/"pkgconfig"
+    ENV.append "CFLAGS", "-I"+`pkg-config --variable includedir libusb-1.0`.chomp
+    ENV.append "LDFLAGS", `pkg-config --libs libusb-1.0`.chomp
+    system "make"
+    bin.install "rpiboot"
+  end
+
+  test do
+    assert_match "rpiboot", shell_output("#{bin}/rpiboot -help 2>&1", 255)
+  end
+end
+
+__END__
+diff --git a/Makefile.orig b/Makefile
+index 6d8dc6a..0a3d4e7 100755
+--- Makefile.orig
++++ Makefile
+@@ -1,5 +1,5 @@
+ rpiboot: main.c msd/bootcode.h msd/start.h
+-	$(CC) -Wall -Wextra -g -o $@ $< -lusb-1.0
++	$(CC) -Wall -Wextra -g -o $@ $< $(CFLAGS) $(LDFLAGS)
+
+ %.h: %.bin ./bin2c
+ 	./bin2c $< $@
